c115842a346153ef0557dd81d2a4db0543b58a8e
projectsaffi-coder <projectsaffi@gmail.com>
2026-04-19 12:36:54 -0400

    fix: remove requestData (SDK mismatch)

--- app/page.tsx
+++ app/page.tsx
@@ -25,12 +25,7 @@ export default function Home() {
           border: "2px solid red",
         }}
       >
-        <AvatarCall
-          connectUrl="/api/session"
-          requestData={{
-            avatarId: "406b979c-0fd3-42e9-9d42-f950406977c2",
-          }}
-        >
+        <AvatarCall connectUrl="/api/session">
           <AvatarVideo />
           <ControlBar />
         </AvatarCall>
